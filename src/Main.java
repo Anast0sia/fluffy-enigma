80a210b7c4a3000310cd32ef6cfa41bdba91a3e1
Nast_programmer <anastypol33@gmail.com>
2024-03-29 19:14:28 +0300

    First task

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.sort(Comparator.naturalOrder());
+        for (int i : intList) {
+            if (i <= 0 || i % 2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
