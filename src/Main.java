7a30b4b418eb95d9e43249a993b4341d83b677dd
Anast0sia <160764898+Anast0sia@users.noreply.github.com>
2024-03-29 20:14:42 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        intList.sort(Comparator.naturalOrder());
-        for (int i : intList) {
-            if (i <= 0 || i % 2 != 0) {
-                continue;
-            }
-            System.out.println(i);
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
         }
+        persons.stream().filter(x -> x.getAge() >= 18).count();
+        persons.stream().filter(x -> x.getSex().equals(Sex.MAN)).filter(x -> x.getAge() >= 18 &&x.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        persons.stream().filter(x -> x.getEducation().equals(Education.HIGHER)).filter(x -> x.getAge() >= 18 && (x.getSex().equals(Sex.MAN) && x.getAge() <= 65) || (x.getSex().equals(Sex.WOMAN) && x.getAge() <= 60))
+                .sorted(Comparator.comparing(Person::getFamily)).collect(Collectors.toList());
     }
 }
